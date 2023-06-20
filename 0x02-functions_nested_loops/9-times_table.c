0d27172cf82f92c3291768f82064f1918bbb5df4
agnhamza <agn11amza@gmail.com>
2023-06-20 12:45:04 -0700

    jobs done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,8 +10,8 @@ void times_table(void){
 	int k =0;
 	for (i = 0 ; i < 10; i++){
 		for (j = 0 ; j < 10;j++){
-			k=k+k;
 	   		tab[i][j]=k;
+			k=k+1;
 			printf("%d, ",tab[i][j]);
 		}
 	printf("\n");
