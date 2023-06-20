429b57ffdcb60738c923b59612041bac6f9b3d7f
agnhamza <agn11amza@gmail.com>
2023-06-20 11:11:52 -0700

    jobs done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		printf("+");
 		return (1);
 	} else if (n == 0)
-}
-		_putchar(48);
+	{
+		printf("0");
 		return (0);
 	} else if (n < 0)
 	{
-		_putchar('-');
-	}
+		printf("-");
 		return (-1);
-}
+	}
+}         
