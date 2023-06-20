368d08d24a595079551c9886b46ff21ef2a1f7fe
agnhamza <agn11amza@gmail.com>
2023-06-20 12:38:26 -0700

    jobs done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,10 +5,11 @@
  * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void){
+	int i,j;
 	int tab[10][10];
 	int k =0;
-	for (int i = 0 ; i < 10; i++){
-		for (int j = 0 ; j < 10;j++){
+	for (i = 0 ; i < 10; i++){
+		for (j = 0 ; j < 10;j++){
 	   		tab[i][j]=k+k;
 			printf("%d, ",k+k);
 		}
